9dd43d18ff67133cc2b4d71273b570b4dcfc4bb4
m.shalamov <m.shalamov@rmabler-co.ru>
2015-06-03 17:37:09 +0300

    render simple tpl

--- textgenerator/functions.py
+++ textgenerator/functions.py
@@ -1,18 +1,30 @@
+# -*- coding: utf-8 -*-
 import pymorphy2
+from pymorphy2.shapes import restore_capitalization
 
 morph = pymorphy2.MorphAnalyzer()
 
 
 def _take_word_form(word, form):
-    for it in morph.parse(word):
-        n = it.inflect({form})
-        if n:
-            return n.word
-        print u'WARNING: unable to take inflection "{}" of word "{}".'.format(form, word)
-    print u'WARNING: unable to parse word "{}". Falling back to its original value'.format(word)
+    try:
+        res = restore_capitalization(
+            morph.parse(word)[0].inflect({form}).word,
+            word
+        )
+        return res
+    except Exception:
+        for it in morph.parse(word):
+            n = it.inflect({form})
+            if n:
+                return n.word
+        print u'WARNING: unable to parse word "{}". Falling back to its original value'.format(word)
     return word
 
 
 def inflect_case(value, case):
     value = unicode(value).split(' ')
     return' '.join([_take_word_form(v.title(), case) for v in value])
+
+
+def lower(value):
+    return value.lower()
--- textgenerator/generator.py
+++ textgenerator/generator.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import yaml
 import random
 import re
@@ -15,14 +16,15 @@ def get_tpl_variables(tpl):
     result = re.finditer("<[^>\n]+>", tpl)
     vars = []
     for match in result:
-        vars.append((match.group()[1:-1], match.span()))
+        vars.append(match.group()[1:-1])
 
     return vars
 
 
 def render_vars(vars, value_vars):
-    for tpl_var in vars[:]:
-        data_var = tpl_var[0].split('~')
+    res = vars[:]
+    for i, tpl_var in enumerate(vars[:]):
+        data_var = tpl_var.split('~')
         var = data_var.pop(0)
         funcs = []
         for it in data_var:
@@ -32,15 +34,19 @@ def render_vars(vars, value_vars):
             print '----------'
             variable = data.pop(0)
             funcs.append([(variable, data)])
-        render_var = var
-        if data_var in value_vars:
-            for func, func_vars in funcs:
-                func = fabric_tplengine_functions(func)
-                if func:
-                    render_var = func()
+
+        if var in value_vars:
+            render_var = value_vars[var]
+            for func in funcs:
+                tpl_func = fabric_tplengine_functions(func[0][0])
+                if tpl_func:
+                    render_var = tpl_func(render_var, *func[0][1])
+            print "arrrrrrrrrrrrrrrr", i, vars[i], render_var
+            res[i] = render_var
         print '==========='
-        print var, funcs
+        print var, funcs, render_var
         print '==========='
+    return res
 
 
 def spintax(value):
@@ -68,10 +74,21 @@ def fabric_tplengine_functions(name_function):
     return func
 
 
+def render_tpl(proposal, render_vars):
+    pattern = re.finditer("<[^>\n]+>", tpl)
+    pattern = re.compile("<[^>\n]+>")
+    res = proposal
+    for r in render_vars:
+        res = pattern.sub(r, res, 1)
+
+    return spintax(res)
+
 if __name__ == '__main__':
     first_proposal = load_proposal('tpl/ex.yaml')
     tpl = first_proposal[0]['values'][1]['value']
     print spintax(tpl)
     vars = get_tpl_variables(tpl)
     print fabric_tplengine_functions('sdfsdfsdfsdfsdsdf')
-    render_vars(vars, {'city': u'Париж', 'distance': '10', 'name': u'Смоуг', 'type': u'Первый'})
+    render = render_vars(vars, {'city': u'Париж', 'distance': '10', 'name': u'Смоуг', 'type': u'Первый'})
+    print render
+    print render_tpl(tpl, render)
